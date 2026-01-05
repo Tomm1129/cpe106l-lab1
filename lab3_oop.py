e5883cf05acc434c89045da7425965c8c5fe1ff3
Tomm1129 <tmalim@mymail.mapua.edu.ph>
2026-01-05 18:35:04 +0800

    Create lab3_oop.py

---
+++ lab3_oop.py
@@ -0,0 +1,18 @@
+class BankAccount:
+  def __init__(self, owner, balance=0):
+      self.owner = owner
+      self.balace = balance
+
+  def deposit(self, amount):
+      self.balance += amount
+
+  def withdraw(self, amount):
+      if amount <= self.balance:
+          self.balance -= amount
+      else:
+          print("Insufficient funds")
+
+account = BankAccount("John", 1000)
+account.deposit(500)
+account.withdraw(300)
+print(account.balance)
