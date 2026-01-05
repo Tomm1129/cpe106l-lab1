6ed6539ed955168494b7cc6a6af38833069eb6a6
Tomm1129 <tmalim@mymail.mapua.edu.ph>
2026-01-05 18:52:49 +0800

    Update lab3_oop.py

--- lab3_oop.py
+++ lab3_oop.py
@@ -1,7 +1,7 @@
 class BankAccount:
   def __init__(self, owner, balance=0):
       self.owner = owner
-      self.balace = balance
+      self.balance = balance
 
   def deposit(self, amount):
       self.balance += amount
